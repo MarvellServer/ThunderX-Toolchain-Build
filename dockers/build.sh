4f2b6774464f5d98b643dc3f965d6fec217a1269
Srikanth Yalavarthi <syalavarthi@marvell.com>
2020-07-01 03:24:31 -0700

    added 502.gcc_r run for testing

--- dockers/build.sh
+++ dockers/build.sh
@@ -27,6 +27,9 @@ cat ${BASE_DIR}/toolchain/Dockerfile.${toolchain_name} >> ${BUILD_DIR}/Dockerfil
 # add package Dockerfile
 cat ${BASE_DIR}/package/Dockerfile.${platform_name} >> ${BUILD_DIR}/Dockerfile
 
+# add apps Dockerfile
+cat ${BASE_DIR}/apps/Dockerfile.spec.${platform_name} >> ${BUILD_DIR}/Dockerfile
+
 # update Dockerfile
 sed -i "s/PLATFORM_NAME/${platform_name}/g"         ${BUILD_DIR}/Dockerfile
 sed -i "s/PLATFORM_VERSION/${platform_version}/g"   ${BUILD_DIR}/Dockerfile
@@ -36,9 +39,11 @@ sed -i "s/TOOLCHAIN_BRANCH/${toolchain_branch}/g"   ${BUILD_DIR}/Dockerfile
 
 # copy extra files
 cd ${BASE_DIR}
-cp -r extras/github_rsa ${BUILD_DIR}/data/github_rsa
-cp -r extras/ssh_config ${BUILD_DIR}/data/config
-cp -r package/llvm.spec ${BUILD_DIR}/data/llvm.spec
+cp -r extras/github_rsa     ${BUILD_DIR}/data/github_rsa
+cp -r extras/ssh_config     ${BUILD_DIR}/data/config
+cp -r package/llvm.spec     ${BUILD_DIR}/data/llvm.spec
+cp -r extras/cpu2017.tar.xz ${BUILD_DIR}/data/cpu2017.tar.xz
+cp -r apps/llvm.cfg         ${BUILD_DIR}/data/llvm.cfg
 
 # build docker
 cd ${BUILD_DIR}
@@ -46,6 +51,6 @@ image_tag=${toolchain_name}_${platform_name}_${platform_version}:${toolchain_bra
 docker build --add-host mirror.fileplanet.com:0.0.0.0 -t ${image_tag} .
 
 # copy the rpm file
-docker run ${image_tag}
-container_id=$(docker ps -a | grep ${image_tag} | head -n1 | awk '{print $1}')
-docker cp ${container_id}:/root/rpmbuild/RPMS/aarch64/llvm-9.0.0-20200701.el8.aarch64.rpm ${BUILD_DIR}/
+#docker run ${image_tag}
+#container_id=$(docker ps -a | grep ${image_tag} | head -n1 | awk '{print $1}')
+#docker cp ${container_id}:/root/rpmbuild/RPMS/aarch64/llvm-9.0.0-20200701.el8.aarch64.rpm ${BUILD_DIR}/
